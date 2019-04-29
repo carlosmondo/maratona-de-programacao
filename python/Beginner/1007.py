0270b6b8c5c4beed97c6f4118e25d28c34f87558
Carlos Mondo <48037173+carlosmondo@users.noreply.github.com>
2019-04-29 16:04:47 -0300

    Create 1007.py

---
+++ python/Beginner/1007.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# 1007 - Difference
+
+a = int(input())
+
+b = int(input())
+
+c = int(input())
+
+d = int(input())
+
+print("DIFERENCA = {0}".format(( a * b ) - ( c * d )))
