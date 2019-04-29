d657c44a2cd2428593bcc2a7ed7d76a4a2755998
Carlos Mondo <48037173+carlosmondo@users.noreply.github.com>
2019-04-29 15:30:24 -0300

    Create 1006.py

---
+++ Beginner/1006.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# 1006 - Average 2 - Sequential - Level 1 
+
+a = float(input()) * 0.2
+
+b = float(input()) * 0.3
+
+c = float(input()) * 0.5
+
+print("MEDIA = {:.1f}".format( a + b + c ))
