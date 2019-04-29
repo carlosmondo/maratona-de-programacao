b4e2dbf92eaaef680045e8b272a30032cd95f457
Carlos Mondo <48037173+carlosmondo@users.noreply.github.com>
2019-04-29 16:24:24 -0300

    Create 1009.py

---
+++ python/Beginner/1009.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# 1009 - Salary with Bonus
+
+nome = input()
+salario = float(input())
+venda = float(input())
+total = s + 0.15 * v
+
+print("TOTAL = R$ {:.2f}".format(total))
