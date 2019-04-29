ecf26146f2e1da8ac622e8e07066bd4333b23f2c
Carlos Mondo <48037173+carlosmondo@users.noreply.github.com>
2019-04-29 14:59:24 -0300

    1003 - Simple Sum
    Deixando o código mais simples

--- Beginner/1003.py
+++ Beginner/1003.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
 # 1003 - Simple Sum - Sequencial - Level 1
-def read_integer():
-    
-    return int(input())
-    
-    
-a = read_integer()
 
-b = read_integer()
+a = int(input())
+
+b = int(input)
 
 soma = (a + b)
 
