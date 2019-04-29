9a3b055dcabefd960445b66573470ab1314bbae0
Carlos Mondo <48037173+carlosmondo@users.noreply.github.com>
2019-04-29 12:20:29 -0300

    Create 1003.py - Simple Sum

---
+++ Beginner/1003.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+def read_integer():
+    
+    return int(input())
+    
+    
+a = read_integer()
+
+b = read_integer()
+
+soma = (a + b)
+
+print("SOMA = {0}".format(soma))
