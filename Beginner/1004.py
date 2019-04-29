ffcd4a054229bed9afc77f2b99a61b63171ec4ef
Carlos Mondo <48037173+carlosmondo@users.noreply.github.com>
2019-04-29 12:28:36 -0300

    1004 - Simple Product

---
+++ Beginner/1004.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# 1004 - Simple Product - Sequential - Level 1
+def return_integer():
+    return int(input())
+    
+    
+a = return_integer()
+b = return_integer()
+prod = a*b
+print("PROD = {0}".format(prod))
