b07ae2e3d3b6e75e20a64d39337c1ba8ccd604c0
Carlos Mondo <48037173+carlosmondo@users.noreply.github.com>
2019-04-29 15:16:51 -0300

    Create 1005.py

---
+++ Beginner/1005.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# 1005 - Average 1 - Sequential - Level 1
+a = 3.5 * float(input())
+
+b = 7.5 * float(input())
+
+media = ( a + b ) / 11
+
+print("MEDIA = {:.5f}".format(media))
